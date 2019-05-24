d125b523eb657f34139777c234b0a6f5a09076a9
Ali AslRousta <aslrousta@gmail.com>
2019-05-24 14:02:32 +0430

    Use Postgres as default storage

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,32 +5,25 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-pg/pg"
 	"github.com/google/logger"
 
 	"gorum/pkg/api"
 	"gorum/pkg/auth"
-	"gorum/pkg/store/mem"
+	"gorum/pkg/store/postgres"
 )
 
 func main() {
 	log := logger.Init("main", true, false, os.Stdout)
 
-	host := os.Getenv("HOST")
-	if host == "" {
-		host = "localhost"
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	db := initDatabase()
 
 	userService := auth.UserService{
-		Storer: &mem.UserStore{},
+		Storer: postgres.NewUserStorer(db),
 	}
 
 	sessionService := auth.SessionService{
-		Storer: &mem.SessionStore{},
+		Storer: postgres.NewSessionStorer(db),
 	}
 
 	router, err := api.Build(userService, sessionService)
@@ -38,7 +31,36 @@ func main() {
 		log.Fatal(err)
 	}
 
-	addr := fmt.Sprintf("%s:%s", host, port)
+	addr := hostAddress()
 	log.Infof("starting server on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, router))
+}
+
+func hostAddress() string {
+	host := envWithDefault("HOST", "localhost")
+	port := envWithDefault("PORT", "8080")
+
+	return fmt.Sprintf("%s:%s", host, port)
+}
+
+func initDatabase() *pg.DB {
+	host := envWithDefault("DB_HOST", "localhost")
+	port := envWithDefault("DB_PORT", "5432")
+	name := envWithDefault("DB_NAME", "gorum")
+	user := envWithDefault("DB_USER", "postgres")
+	pass := envWithDefault("DB_PASS", "")
+
+	return pg.Connect(&pg.Options{
+		Addr:     fmt.Sprintf("%s:%s", host, port),
+		User:     user,
+		Password: pass,
+		Database: name,
+	})
+}
+
+func envWithDefault(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
 }
