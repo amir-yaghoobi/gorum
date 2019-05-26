93b53aa2ec3072b192f22322ea27ba8f6cf66cec
Ali AslRousta <aslrousta@gmail.com>
2019-05-26 08:13:37 +0430

    Change `SERIAL` columns to `BIGSERIAL`

--- pkg/store/postgres/migration/001_create_users_table.go
+++ pkg/store/postgres/migration/001_create_users_table.go
@@ -6,13 +6,13 @@ func init() {
 	migrations.MustRegister(func(db migrations.DB) error {
 		_, err := db.Exec(`
 			CREATE TABLE users (
-				id            SERIAL       PRIMARY KEY,
+				id            BIGSERIAL    PRIMARY KEY,
 				name          VARCHAR(255) NOT NULL UNIQUE,
 				email         VARCHAR(255) NOT NULL UNIQUE,
 				secret        VARCHAR(255) NOT NULL,
 				full_name     VARCHAR(255) NOT NULL,
 				tag_line      VARCHAR(255),
-				role          INTEGER,
+				role          INTEGER      NOT NULL,
 				respect       INTEGER      NOT NULL DEFAULT '0',
 				registered_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
 				updated_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
